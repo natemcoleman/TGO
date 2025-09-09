4895a31e9b5d1a5556bf3c8769534170f549f2ff
natemcoleman <natecolemancv@gmail.com>
2025-09-08 22:23:24 -0400

    upates

--- TGO/ContentView.swift
+++ TGO/ContentView.swift
@@ -15,15 +15,15 @@ struct ContentView: View {
         TabView {
             HomeView().tabItem { Image(systemName: "house")
             }.tag(1)
-                .ignoresSafeArea()
-            AnalyticsView().tabItem {
-                Image(systemName: "chart.bar.xaxis")
-            }.tag(2)
-                .ignoresSafeArea()
+//                .ignoresSafeArea()
+//            AnalyticsView().tabItem {
+//                Image(systemName: "chart.bar.xaxis")
+//            }.tag(2)
+//                .ignoresSafeArea()
             RouteView2().tabItem {
                 Image(systemName: "point.bottomleft.forward.to.point.topright.filled.scurvepath")
             }.tag(2)
-                .ignoresSafeArea()
+//                .ignoresSafeArea()
         }
         .ignoresSafeArea()
     }
